cda8b499bac49b462e62f4c3532344bb0681193a
system sylvere BARINDA <systembarinda@gmail.com>
2026-05-31 16:12:40 +0200

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-import ActivityPanel from './components/ActivityPanel'
-import Navbar from './components/Navbar'
-import OverviewCards from './components/OverviewCards'
-import Sidebar from './components/Sidebar'
-import TopBar from './components/TopBar'
-
 function App() {
   return (
-    <div className="min-h-screen bg-[#F7F6F4] text-slate-400">
-      <div className='border h-100 w-100'> done</div>
-      <div className='w-full'> <Navbar /></div>
+    <div className="min-h-screen bg-[#F7F6F4] text-slate-400 flex">
+      <div className="sideBar border h-screen w-[30%]">
+        <h1>Sidebar</h1>
+      </div>
+
+      <div className="border h-screen w-[70%]">
+        <h1>Main Content</h1>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
