22989861f1bdb477939316e450d22306ec8dbf76
system sylvere BARINDA <systembarinda@gmail.com>
2026-05-31 16:20:12 +0200

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
+import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className="min-h-screen bg-[#F7F6F4] text-slate-400 flex">
-      <div className="sideBar border h-screen w-[20%]">
+      <div className="sideBar border h-full w-[20%]">
         <Sidebar />
       </div>
 
-      <div className="border h-screen w-[80%]">
-        <h1>Main Content</h1>
+      <div className="border h-full w-[80%]">
+        <div><Navbar /></div>
+
+        <br />
+
+        
       </div>
     </div>
   );
