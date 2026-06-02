6d96a7b5ae3eb20ba6683ecdd67cfd68f2da46c2
system sylvere BARINDA <systembarinda@gmail.com>
2026-06-02 22:13:10 +0200

    done

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,11 @@ import TopBar from "./components/TopBar";
 
 function App() {
   return (
-    <div className="min-h-screen bg-[#F7F6F4] text-slate-400 flex">
+    <div className="min-h-screen bg-[#F7F6F4] text-slate-400">
+      <div className="mobileMun flex border h-full w-full text-slate-800">
+        <div className="menuRight">done</div>
+        <div className="menuLeft">done</div>
+      </div>
       <div className="sideBar border h-full w-[20%]">
         <Sidebar />
       </div>
