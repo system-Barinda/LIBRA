0a5745a6b71395642efb3cc61d5c0e04cccc7b2e
system sylvere BARINDA <systembarinda@gmail.com>
2026-06-02 22:00:52 +0200

    done

---
+++ src/components/Activities.tsx
@@ -0,0 +1,7 @@
+export default function Activities() {
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <h1 className="text-4xl font-bold text-green-500">Activities</h1>
+        </div>
+    )
+}
