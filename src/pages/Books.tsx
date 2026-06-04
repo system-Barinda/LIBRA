3b8bd6e20e33ce99552d482c3d19a3b3b0d8d13d
system sylvere BARINDA <systembarinda@gmail.com>
2026-06-04 23:57:16 +0200

    chore:done to update

---
+++ src/pages/Books.tsx
@@ -0,0 +1,7 @@
+export default function Books() {
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <h1 className="text-4xl font-bold text-green-500">Books</h1>
+        </div>
+    )
+}
