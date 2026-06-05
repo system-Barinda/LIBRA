92b8223cdc57a852881205e47dc70709814b658f
system sylvere BARINDA <systembarinda@gmail.com>
2026-06-05 17:02:38 +0200

    chore:done'

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,16 +1,46 @@
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
 
 export default function MainLayout() {
-      const [showMobileMenu, setShowMobileMenu] = React.useState(false);
-       const toggleMobileMenu = () => {
-    setShowMobileMenu(!showMobileMenu);
-  };
-  return (
-    <div className="app-layout">
-       <Sidebar closeMenu={() => setShowMobileMenu(false)} />
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
 
-      <main className="content">
+  return (
+    <div className="flex min-h-screen bg-slate-100">
+      {/* Desktop Sidebar */}
+      <div className="hidden lg:block w-72 shrink-0">
+        <Sidebar />
+      </div>
+
+      {/* Mobile Sidebar Overlay */}
+      {showMobileMenu && (
+        <div className="fixed inset-0 z-50 lg:hidden">
+          <div
+            className="absolute inset-0 bg-black/50"
+            onClick={() => setShowMobileMenu(false)}
+          />
+
+          <div className="relative w-72 h-full">
+            <Sidebar
+              isMobile={true}
+              closeMenu={() => setShowMobileMenu(false)}
+            />
+          </div>
+        </div>
+      )}
+
+      {/* Main Content */}
+      <main className="flex-1 overflow-auto">
+        {/* Mobile Menu Button */}
+        <div className="lg:hidden p-4">
+          <button
+            onClick={() => setShowMobileMenu(true)}
+            className="rounded-lg bg-slate-800 px-4 py-2 text-white"
+          >
+            Menu
+          </button>
+        </div>
+
         <Outlet />
       </main>
     </div>
