6707a64b30f9175b50c20ec71d6c6131e7506167
system sylvere BARINDA <systembarinda@gmail.com>
2026-06-20 21:43:38 +0200

    done

--- src/pages/Inbox.tsx
+++ src/pages/Inbox.tsx
@@ -4,25 +4,41 @@ import EmailViewer from "../components/inbox/EmailViewer";
 
 export default function Inbox() {
   return (
-    <div className="min-h-screen bg-[#f4f4f4] p-3 md:p-0">
-      <div className="mx-auto flex h-[calc(100vh-24px)] w-full overflow-hidden rounded-[28px] bg-white shadow-lg md:h-screen  md:rounded-none md:shadow-none">
-
-        {/* Categories */}
-        <div className="hidden w-[260px] border-r border-gray-200 bg-white md:block">
-          <Categories />
-        </div>
-
-        {/* Email List */}
-        <div className="w-full  border-r border-gray-900 bg-[#fafafa]">
-          <EmailList />
-        </div>
-
-        {/* Email Viewer */}
-        <div className="hidden flex-1 bg-white lg:block">
-          <EmailViewer />
-        </div>
-
-      </div>
+    <div className="border border-gray-900 h-screen bg-[#f4f4f4]">
+     
     </div>
   );
 }
+
+
+
+
+
+// import Categories from "../components/inbox/Categories";
+// import EmailList from "../components/inbox/EmailList";
+// import EmailViewer from "../components/inbox/EmailViewer";
+
+// export default function Inbox() {
+//   return (
+//     <div className="border border-gray-900 min-h-screen bg-[#f4f4f4] p-3 md:p-0">
+//       <div className="border mx-auto flex h-[calc(100vh-24px)] w-full overflow-hidden rounded-[28px] bg-red-800 shadow-lg md:h-screen  md:rounded-none md:shadow-none">
+
+//         {/* Categories */}
+//         <div className="hidden w-[260px] border-r border-gray-200 bg-white md:block">
+//           <Categories />
+//         </div>
+
+//         {/* Email List */}
+//         <div className="w-full  border-r border-gray-200 bg-[#fafafa]">
+//           <EmailList />
+//         </div>
+
+//         {/* Email Viewer */}
+//         <div className="hidden flex-1 bg-white lg:block">
+//           <EmailViewer />
+//         </div>
+
+//       </div>
+//     </div>
+//   );
+// }
