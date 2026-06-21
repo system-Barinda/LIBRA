bc858e4b9d9e4ac56d76c03f92b42b037f12fc6e
system sylvere BARINDA <systembarinda@gmail.com>
2026-06-21 19:34:40 +0200

    done'

--- src/pages/Inbox.tsx
+++ src/pages/Inbox.tsx
@@ -4,23 +4,23 @@ import EmailViewer from "../components/inbox/EmailViewer";
 
 export default function Inbox() {
   return (
-    <div className="border border-gray-900 h-screen bg-[#f4f4f4] flex">
-     <div className="sidecontainer border h-screen w-[20%] text-black">
+    <div className="border border-gray-900 h-screen bg-[#f4f4f4] flex flex-col">
+     <div className="sidecontainer border h-screen lg:w-[20%] text-black">
       
       {/* Categories */}
            <Categories />
 
         </div>
 
-     <div className="rightcontainer border h-screen w-[80%] text-black flex">
+     <div className="rightcontainer border h-screen w-full lg:w-[80%] text-black flex flex-col">
                {/* Email List */}
-                  <div className="w-[30%]  border-r border-gray-200 bg-[#fafafa]">
+                  <div className="w-full lg:w-[30%]  border-r border-gray-200 bg-[#fafafa]">
                        <EmailList />
                   </div>
 
 
                 {/* Email Viewer */}
-                   <div className="w-[70%] bg-white">
+                   <div className="w-full lg:w-[70%] bg-white">
                     <EmailViewer />
                   </div>
      </div>
@@ -28,35 +28,3 @@ export default function Inbox() {
   );
 }
 
-
-
-
-
-// import Categories from "../components/inbox/Categories";
-// import EmailList from "../components/inbox/EmailList";
-// import EmailViewer from "../components/inbox/EmailViewer";
-
-// export default function Inbox() {
-//   return (
-//     <div className="border border-gray-900 min-h-screen bg-[#f4f4f4] p-3 md:p-0">
-//       <div className="border mx-auto flex h-[calc(100vh-24px)] w-full overflow-hidden rounded-[28px] bg-red-800 shadow-lg md:h-screen  md:rounded-none md:shadow-none">
-
-//         {/* Categories */}
-//         <div className="hidden w-[260px] border-r border-gray-200 bg-white md:block">
-//           <Categories />
-//         </div>
-
-//         {/* Email List */}
-//         <div className="w-full  border-r border-gray-200 bg-[#fafafa]">
-//           <EmailList />
-//         </div>
-
-//         {/* Email Viewer */}
-//         <div className="hidden flex-1 bg-white lg:block">
-//           <EmailViewer />
-//         </div>
-
-//       </div>
-//     </div>
-//   );
-// }
