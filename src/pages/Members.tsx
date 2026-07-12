43aea61cc62c7ecdb58389fea8491147bec67cf3
system sylvere BARINDA <systembarinda@gmail.com>
2026-07-12 20:35:16 +0200

    done

---
+++ src/pages/Members.tsx
@@ -0,0 +1,11 @@
+import MembersTable from "../components/Members/MembersTable";
+
+const Members = () => {
+  return (
+    <div className="min-h-screen bg-gray-50 p-4 lg:p-6">
+      <MembersTable />
+    </div>
+  );
+};
+
+export default Members;
