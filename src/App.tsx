255a47ecaf8476ece1bf96688f45ce0f5d5c5420
system sylvere BARINDA <systembarinda@gmail.com>
2026-07-24 20:21:15 +0200

    done

--- src/App.tsx
+++ src/App.tsx
@@ -16,10 +16,10 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Register />} />
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route element={<MainLayout />}>
-          <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/inbox" element={<Inbox />} />
           <Route path="/activity" element={<LibraryActivity />} />
           <Route path="/books" element={<Books />} />
